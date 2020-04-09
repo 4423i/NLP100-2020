89ea8826558f20eecb42be844c91f8439251c9e8
Aoi <ao.oz.725@gmail.com>
2020-04-09 15:26:31 +0900

    update

--- 02/10.py
+++ 02/10.py
@@ -1,6 +1,11 @@
 import csv
 import pprint
+import pandas as pd
 
 with open('popular-names.txt') as f:
     reader = csv.reader(f)
     print(len([row for row in reader]))
+
+#pandasを使う方法，こっちのほうが遅い．    
+#df = pd.read_csv('popular-names.txt')
+#print(len(df))
