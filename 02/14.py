5b651b89ea2dcbea0a5dec656a71c8ea761a876c
Aoi <ao.oz.725@gmail.com>
2020-04-15 21:14:17 +0900

    tailを理解

--- 02/14.py
+++ 02/14.py
@@ -1,6 +1,10 @@
+import sys
 import pandas as pd
 
 csv_file = 'popular-names.txt'
-csv_input = pd.read_csv(csv_file, sep = "\t")
+csv_input = pd.read_csv(csv_file, sep = "\t", header=None)
 
-print(csv_input.head(int(input()) - 1))
+n = int(sys.argv[1])
+#n = int(input())
+
+print(csv_input.head(n))
---
+++ 02/15.sh
@@ -0,0 +1,6 @@
+tail -n $1 popular-names.txt
+
+#or
+#tail -n $1 popular-names.txt
+#read a
+#tail -n $a popular-names.txt
